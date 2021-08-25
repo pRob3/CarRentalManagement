29989f60788416ac3e3382af4f3854105ba2f510
pRob3 <hundraprocent@me.com>
2021-08-25 10:12:05 +0200

    Generic unit of work

---
+++ CarRentalManagement/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using CarRentalManagement.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Save();
+
+        IGenericRepository<Make> Makes { get; }
+        //IGenericRepository<Model> Models { get; }
+        //IGenericRepository<Vehicle> Vehicles { get; }
+
+    }
+}
---
+++ CarRentalManagement/Server/Repository/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using CarRentalManagement.Server.Data;
+using CarRentalManagement.Server.IRepository;
+using CarRentalManagement.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Server.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private IGenericRepository<Make> _makes;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Make> Makes 
+            => _makes ??= new GenericRepository<Make>(_context);
+
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
