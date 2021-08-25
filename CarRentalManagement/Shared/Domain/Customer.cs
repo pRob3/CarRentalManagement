04c43f5229e1e3682790635749917ec45428f620
pRob3 <hundraprocent@me.com>
2021-08-25 05:16:46 +0200

    Added Application Models

---
+++ CarRentalManagement/Shared/Domain/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CarRentalManagement.Shared.Domain
+{
+    public class Customer : BaseDomainModel
+    {
+        public string TaxId { get; set; }
+        public string Adress { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAdress { get; set; }
+        public virtual List<Booking> Bookings { get; set; }
+    }
+}
